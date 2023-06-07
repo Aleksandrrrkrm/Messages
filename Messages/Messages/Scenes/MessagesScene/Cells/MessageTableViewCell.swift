6134d82c2d1ca516c703ef0f8a26cc6ff847928f
Aleksandrrrkrm <golovinkrm@icloud.com>
2023-06-07 16:58:04 +0300

    done

--- Messages/Messages/Scenes/MessagesScene/Cells/MessageTableViewCell.swift
+++ Messages/Messages/Scenes/MessagesScene/Cells/MessageTableViewCell.swift
@@ -9,13 +9,17 @@ import UIKit
 
 final class MessageTableViewCell: BaseMessageTableViewCell {
     
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         setupContentView()
         setupLabel()
         setupProfileImageView()
         setupBaseLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     private func setupBaseLabel() {
